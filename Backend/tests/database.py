a53fc695262abc0f0610dd864e393a388002ca63
Relokko <zalemateo@gmail.com>
2024-10-01 12:03:09 -0300

    Update suscriptor

--- Backend/tests/database.py
+++ Backend/tests/database.py
@@ -7,10 +7,10 @@ from sqlalchemy import StaticPool, create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from src.main import app
 from src.db_models import get_db
-from src.models import BaseModel
-from src.services import crear_nodo
-from src.schemas import NodoCreate 
-from src.models import Nodo
+from src.nodo.models import BaseModel
+from src.nodo.services import crear_nodo
+from src.nodo.schemas import NodoCreate 
+from src.nodo.models import Nodo
 
 load_dotenv()
 
