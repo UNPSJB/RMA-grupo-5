da70b4bcd39b9acf454b52fed3191d1080ed0585
Relokko <zalemateo@gmail.com>
2024-10-03 20:15:20 -0300

    Update Routers y Services para leer ultimo nodo - Probando WebSocket...

--- Backend/src/main.py
+++ Backend/src/main.py
@@ -1,6 +1,8 @@
-import os, sys, json
+import os
+import json
+import asyncio
 from dotenv import load_dotenv
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from datetime import datetime
@@ -42,7 +44,23 @@ app.add_middleware(
 
 # Asociar los routers a nuestra app
 app.include_router(example_router)
- 
+
+# Conexiones activas de WebSockets
+connected_clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    connected_clients.append(websocket)
+    
+    try:
+        while True:
+            # Mantener el WebSocket abierto
+            await websocket.receive_text()
+    except WebSocketDisconnect:
+        connected_clients.remove(websocket)
+
+# Callback
 def mi_callback(mensaje: str) -> None:
     db: Session = SessionLocal()
 
@@ -64,6 +82,10 @@ def mi_callback(mensaje: str) -> None:
         crear_medicion(db, medicion)
         
         
+        # Enviar el nodo a través de WebSocket a los clientes conectados
+        for client in connected_clients:
+            asyncio.create_task(client.send_text(json.dumps(mensaje_dict)))
+
     except Exception as e:
         print(f"Error al procesar el mensaje: {e}")
     finally:
@@ -71,7 +93,7 @@ def mi_callback(mensaje: str) -> None:
 
 # Start the MQTT subscriber in a separate thread
 def run_mqtt_subscriber():
-    sub  = Subscriptor(client=Client(), on_message_callback=mi_callback)
+    sub = Subscriptor(client=Client(), on_message_callback=mi_callback)
     
     # Conectar al broker MQTT usando la configuración
     sub.connect(config.host, config.port, config.keepalive)
@@ -89,11 +111,11 @@ def start_mqtt_thread():
 @app.on_event("startup")
 async def startup_event():
     # Crear las tablas en la base de datos si no existen
-    from src.db_models import Base
-    Base.metadata.create_all(bind=engine)
+    BaseModel.metadata.create_all(bind=engine)
     
     start_mqtt_thread()
 
 if __name__ == "__main__":
     # Ejecutar la aplicación FastAPI con Uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
+
