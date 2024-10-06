1ab5fab95b5d97aadab0468fecc3b99596f660e1
Relokko <zalemateo@gmail.com>
2024-10-06 07:26:07 -0300

    Ordenando rutas y añadiendo nuevas...

--- Backend/src/nodo/router.py
+++ Backend/src/nodo/router.py
@@ -14,12 +14,15 @@ def create_medicion(nodo: schemas.MedicionCreate, db: Session = Depends(get_db))
 
 @router.get("/leer_mediciones", response_model=list[schemas.Medicion])
 def read_mediciones(db: Session = Depends(get_db)):
-    mediciones = services.listar_mediciones(db)
-    return services.listar_mediciones(db)
+    return services.listar_mediciones(db) 
 
 @router.get("/leer_medicion/{medicion_id}", response_model=schemas.Medicion)
 def read_medicion(medicion_id: int, db: Session = Depends(get_db)):
     return services.leer_medicion(db, medicion_id)
+
+@router.get("/leer_mediciones_nodo/{numero_nodo}", response_model=List[schemas.Medicion])
+def read_mediciones_nodo(numero_nodo: int, db: Session = Depends(get_db)):
+    return services.leer_mediciones_nodo(db, numero_nodo)
 
 @router.put("/actualizar_medicion/{medicion_id}", response_model=schemas.Medicion)
 def update_medicion(
@@ -38,21 +41,23 @@ def create_nodo(nodo: schemas.NodoCreate, db: Session = Depends(get_db)):
 
 @router.get("/obtener_nodo/{numero_nodo}", response_model=schemas.Nodo)
 def obtener_nodo(numero_nodo: int, db: Session = Depends(get_db)):
-    try:
-        nodo = services.obtener_nodo(db, numero_nodo)
-        return nodo
-    except exceptions.NodoNoEncontrado:
-        raise HTTPException(status_code=404, detail="Nodo no encontrado")
-    
+    return services.obtener_nodo(db, numero_nodo)
+
 @router.get("/obtener_nodos/", response_model=List[schemas.Nodo])
 def obtener_nodos(db: Session = Depends(get_db)):
     nodos = services.listar_nodos(db)
     return nodos
 
-@router.delete("/eliminar_nodo/{nodo_id}", response_model=schemas.Nodo)
-def delete_nodo(nodo_id: int, db: Session = Depends(get_db)):
-    return services.eliminar_nodo(db, nodo_id)
-
 @router.get("/leer_ultima_medicion", response_model=schemas.Medicion)
 def read_ultimo_nodo(db: Session = Depends(get_db)):
     return services.leer_ultima_medicion(db)
+
+@router.put("/actualizar_nodo/{numero_nodo}", response_model=schemas.Nodo)
+def update_nodo(
+    numero_nodo: int, nodo:schemas.NodoUpdate, db: Session = Depends(get_db)
+):
+    return services.modificar_nodo(db, numero_nodo)
+    
+@router.delete("/eliminar_nodo/{nodo_id}", response_model=schemas.Nodo)
+def delete_nodo(nodo_id: int, db: Session = Depends(get_db)):
+    return services.eliminar_nodo(db, nodo_id)
