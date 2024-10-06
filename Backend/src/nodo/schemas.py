9498ba319f0dab65c87d1c7d7eaeea579c9447f7
Relokko <zalemateo@gmail.com>
2024-10-06 06:30:14 -0300

    Agregando correcciones...

--- Backend/src/nodo/schemas.py
+++ Backend/src/nodo/schemas.py
@@ -48,16 +48,3 @@ class NodoUpdate(NodoBase):
 
 class Nodo(NodoBase):
     numero: int
-
-# Para validar que el tipo sea uno de los permitidos
-def validate_type(value: int) -> int:
-    valid_types = [
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
-        11, 12, 13, 14, 15, 16, 17, 18, 
-        19, 20, 21, 22, 23, 24, 25, 26, 
-        27, 28, 29, 30, 31, 32, 33, 34, 
-        35
-    ]
-    if value not in TipoDato:
-        raise ValueError(f"Tipo de dato invalido: {value}. Debe ser un: {TipoDato}.")
-    return value
