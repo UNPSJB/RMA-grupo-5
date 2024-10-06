b3f1965e353975ec4ada985511cfbb3d502da4b7
Relokko <zalemateo@gmail.com>
2024-10-06 12:30:49 -0300

    Update Headers - Grafico principal

--- Frontend/src/views/Index.js
+++ Frontend/src/views/Index.js
@@ -13,64 +13,71 @@ import {
   Row,
   Col,
 } from "reactstrap";
-import { chartOptions, parseOptions, chartExample1, chartExample2 } from "variables/charts.js";
+
+// core components
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2,
+} from "variables/charts.js";
 import Header from "components/Headers/Header.js";
 
 const Index = (props) => {
   const [activeNav, setActiveNav] = useState(1);
-  const [nodoData, setNodoData] = useState(null);
+  const [nodoData, setMedicionData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [averagesList, setAveragesList] = useState([]); // Lista de promedios
-
-  // Función para obtener el día de la semana a partir de la fecha
-  const getDayOfWeek = (dateString) => {
-    const date = new Date(dateString);
-    const days = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
-    return days[date.getDay()];
-  };
+  const [nodos, setNodos] = useState([]);
+  const [nodoSeleccionado, setNodoSeleccionado] = useState(null);
 
   useEffect(() => {
-    const getNodoData = async () => {
+    const getNodos = async () => {
+      setLoading(true); // Asegúrate de iniciar la carga
       try {
-        const response = await fetch("http://localhost:8000/leer_nodos");
+        const response = await fetch("http://localhost:8000/obtener_nodos");
         if (!response.ok) {
-          throw new Error("Error al hacer el fetch");
+          throw new Error("Error al hacer el fetch de nodos");
         }
-        const data = await response.json(); 
-        setNodoData(data); 
-
-        // Agrupar valores por día
-        const groupedByDay = data.reduce((acc, nodo) => {
-          const dayOfWeek = getDayOfWeek(nodo.time);
-          if (!acc[dayOfWeek]) {
-            acc[dayOfWeek] = [];
-          }
-          acc[dayOfWeek].push(parseFloat(nodo.data));
-          return acc;
-        }, {});
-
-        // Calcular promedios
-        const averages = Object.keys(groupedByDay).map((day) => {
-          const values = groupedByDay[day];
-          const sum = values.reduce((a, b) => a + b, 0);
-          const avg = (sum / values.length).toFixed(1);
-          return parseFloat(avg); // Solo el promedio como número
-        });
-
-        setAveragesList(averages); // Actualizar el estado con la lista de promedios
-        setLoading(false);
+        const data = await response.json();
+        setNodos(data);
       } catch (error) {
-        console.error("Error cargando los datos", error);
+        console.error("Error cargando los nodos", error);
         setError(error);
-        setLoading(false);
+      } finally {
+        setLoading(false); // Finaliza la carga
       }
     };
-    getNodoData();
+    getNodos();
   }, []);
 
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error loading data: {error.message}</p>;
+  useEffect(() => {
+    const getMedicionData = async () => {
+      if (nodoSeleccionado) {
+        setLoading(true); // Inicia la carga de mediciones
+        try {
+          const response = await fetch(`http://localhost:8000/leer_mediciones_nodo/${nodoSeleccionado}`);
+          if (!response.ok) {
+            throw new Error("Error al hacer el fetch de mediciones");
+          }
+          const data = await response.json();
+          setMedicionData(data);
+        } catch (error) {
+          console.error("Error cargando los datos", error);
+          setError(error);
+        } finally {
+          setLoading(false); // Finaliza la carga
+        }
+      }
+    };
+    getMedicionData();
+  }, [nodoSeleccionado]);
+
+  // Manejo de carga y errores
+  if (loading) return <p>Cargando...</p>;
+  if (error) return <p>Error cargando datos: {error.message}</p>;
+
+  const valoresNodos = nodoData ? nodoData.map(item => parseFloat(parseFloat(item.data).toFixed(1))) : [];
 
   if (window.Chart) {
     parseOptions(Chart, chartOptions());
@@ -84,8 +91,24 @@ const Index = (props) => {
   return (
     <>
       <Header />
-      {/* Page content */}
-      <Container className="mt--7" fluid>
+      <Container className="mt--9" fluid>
+        <Row className="mt-5 mb-2">
+          <Col xl="2">
+            <h2>Seleccionar Nodo:</h2>
+            <select
+              value={nodoSeleccionado || ""}
+              onChange={(e) => setNodoSeleccionado(e.target.value)}
+              className="form-control"
+            >
+              <option value="" disabled>Seleccione un nodo</option>
+              {nodos.map((nodo, index) => (
+                <option key={index} value={nodo.numero}>
+                  Nodo {nodo.numero}
+                </option>
+              ))}
+            </select>
+          </Col>
+        </Row>
         <Row>
           <Col className="mb-5 mb-xl-0" xl="8">
             <Card className="bg-gradient-default shadow">
@@ -101,9 +124,7 @@ const Index = (props) => {
                     <Nav className="justify-content-end" pills>
                       <NavItem>
                         <NavLink
-                          className={classnames("py-2 px-3", {
-                            active: activeNav === 1,
-                          })}
+                          className={classnames("py-2 px-3", { active: activeNav === 1 })}
                           href="#pablo"
                           onClick={(e) => toggleNavs(e, 1)}
                         >
@@ -113,9 +134,7 @@ const Index = (props) => {
                       </NavItem>
                       <NavItem>
                         <NavLink
-                          className={classnames("py-2 px-3", {
-                            active: activeNav === 2,
-                          })}
+                          className={classnames("py-2 px-3", { active: activeNav === 2 })}
                           data-toggle="tab"
                           href="#pablo"
                           onClick={(e) => toggleNavs(e, 2)}
@@ -129,10 +148,9 @@ const Index = (props) => {
                 </Row>
               </CardHeader>
               <CardBody>
-                {/* Chart */}
                 <div className="chart">
                   <Line
-                    data={activeNav === 1 ? chartExample1.data1(averagesList) : chartExample1.data2(averagesList)}
+                    data={activeNav === 1 ? chartExample1.data1(valoresNodos) : chartExample1.data2(valoresNodos)}
                     options={chartExample1.options}
                     getDatasetAtEvent={(e) => console.log(e)}
                   />
@@ -153,7 +171,6 @@ const Index = (props) => {
                 </Row>
               </CardHeader>
               <CardBody>
-                {/* Chart */}
                 <div className="chart">
                   <Bar
                     data={chartExample2.data}
@@ -165,25 +182,27 @@ const Index = (props) => {
           </Col>
         </Row>
 
-        {/* Nueva sección para mostrar los promedios por día */}
         <Row className="mt-5">
           <Col>
-            <h2>Promedios por Día</h2>
+            <h2>Valores de Nodos (Data)</h2>
             <ul>
-              {averagesList.map((avg, index) => (
-                <li key={index}>Día {index + 1}: {avg}</li>
+              {valoresNodos.map((valor, index) => (
+                <li key={index}>Valor Nodo {index + 1}: {valor}</li>
               ))}
             </ul>
           </Col>
         </Row>
 
-        {/* Mostrar los datos JSON formateados */}
         <Row className="mt-5">
-          <Col>
+          <div>
             <h2>Datos JSON:</h2>
             <pre>{JSON.stringify(nodoData, null, 2)}</pre>
-          </Col>
+          </div>
         </Row>
       </Container>
     </>
+  );
+};
+
+export default Index;
 
