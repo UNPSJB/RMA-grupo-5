45d59831fdb47b1516283d928e398ceea01857af
Yami <yamicollado@hotmail.com>
2024-10-09 19:26:34 -0300

    correccion de errores del formulario

--- Frontend/src/routes.js
+++ Frontend/src/routes.js
@@ -1,6 +1,7 @@
 import Index from "views/Index.js";
 import Maps from "views/examples/Maps.js";
 import Tables from "views/examples/Tables.js";
+import Registrar_nodo from "views/examples/Registrar_nodo.js";
 
 var routes = [
   
@@ -11,6 +12,15 @@ var routes = [
     component: <Index />,
     layout: "/admin",
   },
+  
+  {
+    path: "/registrar_nodo",
+    name: "Registrar nodo",
+    icon: "ni ni-tv-2 text-primary",
+    component: <Registrar_nodo />,
+    layout: "/admin",
+  },
+
   {
     path: "/tables",
     name: "Tablas",
