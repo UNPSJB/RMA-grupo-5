1550f3c4d01c7177af95e7350dca50f42907d74d
Relokko <zalemateo@gmail.com>
2024-10-17 19:38:22 -0300

    Update

--- Frontend/src/components/Navbars/AdminNavbar.js
+++ Frontend/src/components/Navbars/AdminNavbar.js
@@ -40,44 +40,6 @@ const AdminNavbar = (props) => {
               </InputGroup>
             </FormGroup>
           </Form>*/}
-          <Nav className="align-items-center d-none d-md-flex" navbar>
-            <UncontrolledDropdown nav>
-              <DropdownToggle className="pr-0" nav>
-                <Media className="align-items-center">
-                  <span className="avatar avatar-sm rounded-circle">
-                    <img
-                      alt="..."
-                      src={require("../../assets/img/theme/team-4-800x800.jpg")}
-                    />
-                  </span>
-                  <Media className="ml-2 d-none d-lg-block">
-                    <span className="mb-0 text-sm font-weight-bold">
-                      Tomas "Duende" Fernandez
-                    </span>
-                  </Media>
-                </Media>
-              </DropdownToggle>
-              <DropdownMenu className="dropdown-menu-arrow" right>
-                <DropdownItem className="noti-title" header tag="div">
-                  <h6 className="text-overflow m-0">¡Bienvenido!</h6>
-                </DropdownItem>
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-single-02" />
-                  <span>Mi Perfil</span>
-                </DropdownItem>
-                
-                <DropdownItem to="/admin/user-profile" tag={Link}>
-                  <i className="ni ni-support-16" />
-                  <span>Ayuda</span>
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem href="#pablo" onClick={(e) => e.preventDefault()}>
-                  <i className="ni ni-user-run" />
-                  <span>Cerrar Sesión</span>
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
         </Container>
       </Navbar>
     </>
