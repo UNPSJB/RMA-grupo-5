7824c85108dc994607fcc844aac2c8940b6ce88f
Relokko <zalemateo@gmail.com>
2024-10-29 22:48:09 -0300

    Agregado: leer mediciones correctas/erroneas, obtener nodos activos/inactivos

--- Backend/src/nodo/router.py
+++ Backend/src/nodo/router.py
@@ -19,9 +19,17 @@ def read_mediciones(db: Session = Depends(get_db)):
 def read_medicion(medicion_id: int, db: Session = Depends(get_db)):
     return services.leer_medicion(db, medicion_id)
 
-@router.get("/leer_mediciones_nodo/{numero_nodo}", response_model=List[schemas.Medicion])
+@router.get("/leer_ultima_medicion", response_model=schemas.Medicion)
+def read_ultimo_nodo(db: Session = Depends(get_db)):
+    return services.leer_ultima_medicion(db)
+
+@router.get("/leer_mediciones_correctas_nodo/{numero_nodo}", response_model=List[schemas.Medicion])
 def read_mediciones_nodo(numero_nodo: int, db: Session = Depends(get_db)):
-    return services.leer_mediciones_nodo(db, numero_nodo)
+    return services.leer_mediciones_correctas_nodo(db, numero_nodo)
+
+@router.get("/leer_mediciones_incorrectas_nodo/{numero_nodo}", response_model=List[schemas.Medicion])
+def read_mediciones_nodo(numero_nodo: int, db: Session = Depends(get_db)):
+    return services.leer_mediciones_erroneas_nodo(db, numero_nodo)
 
 @router.put("/actualizar_medicion/{medicion_id}", response_model=schemas.Medicion)
 def update_medicion(
@@ -42,28 +50,21 @@ def create_nodo(nodo: schemas.NodoCreate, db: Session = Depends(get_db)):
 def obtener_nodo(numero_nodo: int, db: Session = Depends(get_db)):
     return services.obtener_nodo(db, numero_nodo)
 
-@router.get("/obtener_nodos/", response_model=List[schemas.Nodo])
+@router.get("/obtener_nodos_activos/", response_model=List[schemas.Nodo])
 def obtener_nodos(db: Session = Depends(get_db)):
-    nodos = services.listar_nodos(db)
+    nodos = services.listar_nodos_activos(db)
     return nodos
 
-@router.get("/leer_ultima_medicion", response_model=schemas.Medicion)
-def read_ultimo_nodo(db: Session = Depends(get_db)):
-    return services.leer_ultima_medicion(db)
+@router.get("/obtener_nodos_inactivos/", response_model=List[schemas.Nodo])
+def obtener_nodos(db: Session = Depends(get_db)):
+    nodos = services.listar_nodos_inactivos(db)
+    return nodos
 
 @router.put("/actualizar_nodo/{numero_nodo}", response_model=schemas.Nodo)
 def update_nodo(
     numero_nodo: int, nodo: schemas.NodoUpdate, db: Session = Depends(get_db)
 ):
     return services.modificar_nodo(db, numero_nodo, nodo)  
-    
-@router.delete("/eliminar_nodo/{nodo_id}", response_model=schemas.Nodo)
-def delete_nodo(nodo_id: int, db: Session = Depends(get_db)):
-    return services.eliminar_nodo(db, nodo_id)
-
-@router.get("/leer_ultimo_nodo", response_model=schemas.Nodo)
-def read_ultimo_nodo(db: Session = Depends(get_db)):
-    return services.leer_ultimo_nodo(db)
 
 @router.put("/toggle_estado/{numero_nodo}", response_model=schemas.Nodo)
 def toggle_estado_nodo(numero_nodo: int, db: Session = Depends(get_db)):
@@ -72,6 +73,10 @@ def toggle_estado_nodo(numero_nodo: int, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(nodo)
     return nodo
+
+@router.delete("/eliminar_nodo/{nodo_id}", response_model=schemas.Nodo)
+def delete_nodo(nodo_id: int, db: Session = Depends(get_db)):
+    return services.eliminar_nodo(db, nodo_id)
 
 @router.get("/tipo_dato/{nombre}")
 def get_tipo_dato(nombre: str, db: Session = Depends(get_db)):
