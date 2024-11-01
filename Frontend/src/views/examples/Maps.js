4ded0688eccd6ddfb5f410a9dfa6bbefccf31b5c
Relokko <zalemateo@gmail.com>
2024-10-31 21:50:22 -0300

    Modificando icono de los nodos en el mapa

--- Frontend/src/views/examples/Maps.js
+++ Frontend/src/views/examples/Maps.js
@@ -3,24 +3,18 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { Card, Container, Row, Col } from "reactstrap";
-import { useNavigate } from "react-router-dom"; // Importa useNavigate
+import { useNavigate } from "react-router-dom";
 
 import Header from "components/Headers/Header.js";
 
-// Configurar el icono del marcador de Leaflet
+// Eliminar el icono por defecto
 delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require("leaflet/dist/images/marker-icon-2x.png"),
-  iconUrl: require("leaflet/dist/images/marker-icon.png"),
-  shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
-});
-
 
 const MapWrapper = () => {
   const [nodos, setNodos] = useState([]);
-  const navigate = useNavigate(); // Inicializa useNavigate
+  const navigate = useNavigate();
 
-  const position = [-43.583333, -66.000000];
+  const position = [-43.583333, -66.0];
   const bounds = [
     [-45.0, -67.5], // Esquina suroeste de los límites
     [-42.0, -64.0], // Esquina noreste de los límites
@@ -31,8 +25,7 @@ const MapWrapper = () => {
       try {
         const response = await fetch("http://localhost:8000/obtener_nodos_activos");
         const data = await response.json();
-        const nodosActivos = data;
-        setNodos(nodosActivos);
+        setNodos(data);
       } catch (error) {
         console.error("Error al obtener los nodos:", error);
       }
@@ -41,42 +34,65 @@ const MapWrapper = () => {
     fetchNodos();
   }, []);
 
+  return (
+    <MapContainer
+      center={position}
+      zoom={8}
+      minZoom={8}
+      maxZoom={14}
+      maxBounds={bounds}
+      maxBoundsViscosity={1.0}
+      style={{ height: "600px", width: "100%", borderRadius: "10px", border: "2px solid #ccc" }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
 
-    return (
-      <MapContainer
-        center={position}
-        zoom={8}
-        minZoom={8}
-        maxZoom={14}
-        maxBounds={bounds}
-        maxBoundsViscosity={1.0}
-        style={{ height: "600px", width: "100%", borderRadius: "10px", border: "2px solid #ccc" }}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-  
-      {nodos.map((nodo) => (
-        <Marker
-          key={nodo.numero}
-          position={[nodo.ubicacion_x, nodo.ubicacion_y]}
-        >
-          <Popup>
-            Nodo {nodo.numero} - "{nodo.nombre}"<br />
-            Latitud: {parseFloat(nodo.ubicacion_x).toFixed(6)} <br />
-            Longitud: {parseFloat(nodo.ubicacion_y).toFixed(6)} <br />
-            Chubut<br />
-            <button type="submit" className="button-style"
-              onClick={() => navigate("/admin/tables")}
-            >
-              Ver Detalle
-            </button>
-            
-          </Popup>
-        </Marker>
-      ))}
-      </MapContainer>
+      {/* Configuracion del icono para que muestre el nro de nodo*/}
+      {nodos.map((nodo) => {
+        const customIcon = L.divIcon({
+          className: "custom-icon",
+          html: `<div style="
+                  display: flex;
+                  align-items: center;
+                  justify-content: center;
+                  width: 25px;
+                  height: 25px;
+                  border-radius: 50%;
+                  background-color: #2E86C1;
+                  color: white;
+                  font-size: 15px;
+                  font-weight: bold;
+                  border: 2px solid #ccc;
+                ">${nodo.numero}</div>`,
+          iconSize: [30, 30],
+          iconAnchor: [15, 15]
+        });
+
+        return (
+          <Marker
+            key={nodo.numero}
+            position={[nodo.ubicacion_x, nodo.ubicacion_y]}
+            // Agregar el icono nuevo
+            icon={customIcon}
+          >
+            <Popup>
+                Nodo {nodo.numero} - "{nodo.nombre}"<br />
+                Latitud: {parseFloat(nodo.ubicacion_x).toFixed(6)} <br />
+                Longitud: {parseFloat(nodo.ubicacion_y).toFixed(6)} <br />
+                <br />
+                <button
+                  type="submit" className="button-style"
+                  onClick={() => navigate("/admin/tables")}
+                >
+                  Ver Detalle
+                </button>
+            </Popup>
+          </Marker>
+        );
+      })}
+    </MapContainer>
   );
 };
 
@@ -87,7 +103,10 @@ const Maps = () => {
       <Container className="mt--9" fluid>
         <Row>
           <Col lg="12">
-            <Card className="shadow border-0" style={{ padding: "20px", borderRadius: "10px", border: "2px solid #ccc" }}>
+            <Card
+              className="shadow border-0"
+              style={{ padding: "20px", borderRadius: "10px", border: "2px solid #ccc" }}
+            >
               <Container>
                 <h3 className="row justify-content-md-center">Cuenca Sagmata - Nodos Activos</h3>
               </Container>
