36f09d074ad6eb9318f92cff22fff1e3b359aff7
Relokko <151683203+Relokko@users.noreply.github.com>
2024-11-07 00:02:15 -0300

    Update Register.js

--- Frontend/src/views/examples/Register.js
+++ Frontend/src/views/examples/Register.js
@@ -1,4 +1,3 @@
-
 // reactstrap components
 import {
   Button,
@@ -14,8 +13,51 @@ import {
   Row,
   Col,
 } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+import "../../assets/css/login.css";
+import { message } from "antd";
+import axios from "axios"; 
+import React, { useState } from "react";
 
 const Register = () => {
+  const [nombreUsuario, setNombreUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validación simple para los campos de usuario y contraseña
+    if (!nombreUsuario || !contrasena) {
+      message.error("Por favor, complete todos los campos.");
+      return;
+    }
+
+    const datosUsuario = {
+      usuario: nombreUsuario,
+      contrasenia: contrasena,
+    };
+
+    axios.post('http://localhost:8000/iniciar_sesion', datosUsuario)
+      .then(response => {
+        message.success("Inicio de sesión exitoso");
+
+        // Reiniciar los campos después de un inicio de sesión exitoso
+        setNombreUsuario('');
+        setContrasena('');
+        
+        // Redirigir a la página principal o de administración
+        navigate("/admin/index");
+      })
+      .catch(error => {
+        if (error.response && error.response.data) {
+          message.error(error.response.data.detail || "Error en el inicio de sesión");
+        } else {
+          message.error("Error en el inicio de sesión, verifique sus datos");
+        }
+      });
+  };
+
   return (
     <>
       <Col lg="6" md="8">
@@ -25,7 +67,6 @@ const Register = () => {
               <small>Registrarse con</small>
             </div>
             <div className="text-center">
-
               <Button
                 className="btn-neutral btn-icon"
                 color="default"
@@ -35,10 +76,8 @@ const Register = () => {
                 <span className="btn-inner--icon">
                   <img
                     alt="..."
-                    src={
-                      require("../../assets/img/icons/common/google.svg")
-                        .default
-                    }
+                    src={require("../../assets/img/icons/common/google.svg")
+                      .default}
                   />
                 </span>
                 <span className="btn-inner--text">Google</span>
@@ -46,31 +85,19 @@ const Register = () => {
             </div>
           </CardHeader>
           <CardBody className="px-lg-5 py-lg-5">
-            <div className="text-center text-muted mb-4">
-              <small>O registrarse con sus datos</small>
-            </div>
-            <Form role="form">
+            <Form role="form" onSubmit={handleSubmit}>
               <FormGroup>
                 <InputGroup className="input-group-alternative mb-3">
                   <InputGroupAddon addonType="prepend">
                     <InputGroupText>
-                      <i className="ni ni-hat-3" />
+                      <i className="fa fa-user" />
                     </InputGroupText>
                   </InputGroupAddon>
-                  <Input placeholder="Name" type="text" />
-                </InputGroup>
-              </FormGroup>
-              <FormGroup>
-                <InputGroup className="input-group-alternative mb-3">
-                  <InputGroupAddon addonType="prepend">
-                    <InputGroupText>
-                      <i className="ni ni-email-83" />
-                    </InputGroupText>
-                  </InputGroupAddon>
-                  <Input
-                    placeholder="Email"
-                    type="email"
-                    autoComplete="new-email"
+                  <Input 
+                    placeholder="Usuario" 
+                    type="text"
+                    value={nombreUsuario}
+                    onChange={(e) => setNombreUsuario(e.target.value)} 
                   />
                 </InputGroup>
               </FormGroup>
@@ -81,44 +108,19 @@ const Register = () => {
                       <i className="ni ni-lock-circle-open" />
                     </InputGroupText>
                   </InputGroupAddon>
-                  <Input
-                    placeholder="Password"
-                    type="password"
+                  <Input 
+                    placeholder="Contraseña" 
+                    type="password" 
                     autoComplete="new-password"
+                    value={contrasena}
+                    onChange={(e) => setContrasena(e.target.value)} 
                   />
                 </InputGroup>
               </FormGroup>
-              <div className="text-muted font-italic">
-                <small>
-                  password strength:{" "}
-                  <span className="text-success font-weight-700">strong</span>
-                </small>
-              </div>
-              <Row className="my-4">
-                <Col xs="12">
-                  <div className="custom-control custom-control-alternative custom-checkbox">
-                    <input
-                      className="custom-control-input"
-                      id="customCheckRegister"
-                      type="checkbox"
-                    />
-                    <label
-                      className="custom-control-label"
-                      htmlFor="customCheckRegister"
-                    >
-                      <span className="text-muted">
-                        Estoy de acuerdo con{" "}
-                        <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                          Privacy Policy
-                        </a>
-                      </span>
-                    </label>
-                  </div>
-                </Col>
-              </Row>
+
               <div className="text-center">
-                <Button className="mt-4" color="primary" type="button">
-                  Crear cuenta
+                <Button className="mt-4" color="primary" type="submit">
+                  Iniciar sesión
                 </Button>
               </div>
             </Form>
@@ -130,3 +132,4 @@ const Register = () => {
 };
 
 export default Register;
+
