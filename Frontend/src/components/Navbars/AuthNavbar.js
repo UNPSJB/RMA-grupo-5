1e5b085685b731e07983548d8cdacad278bad3f8
Relokko <151683203+Relokko@users.noreply.github.com>
2024-11-07 00:01:25 -0300

    Update AuthNavbar.js

--- Frontend/src/components/Navbars/AuthNavbar.js
+++ Frontend/src/components/Navbars/AuthNavbar.js
@@ -46,38 +46,7 @@ const AdminNavbar = () => {
               </Row>
             </div>
             <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink className="nav-link-icon" to="/" tag={Link}>
-                  <i className="ni ni-planet" />
-                  <span className="nav-link-inner--text">Tabla de Datos</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  className="nav-link-icon"
-                  to="/auth/register"
-                  tag={Link}
-                >
-                  <i className="ni ni-circle-08" />
-                  <span className="nav-link-inner--text">Registrarse</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink className="nav-link-icon" to="/auth/login" tag={Link}>
-                  <i className="ni ni-key-25" />
-                  <span className="nav-link-inner--text">Iniciar Sesión</span>
-                </NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink
-                  className="nav-link-icon"
-                  to="/admin/user-profile"
-                  tag={Link}
-                >
-                  <i className="ni ni-single-02" />
-                  <span className="nav-link-inner--text">Mi perfil</span>
-                </NavLink>
-              </NavItem>
+
             </Nav>
           </UncontrolledCollapse>
         </Container>
@@ -87,3 +56,4 @@ const AdminNavbar = () => {
 };
 
 export default AdminNavbar;
+
