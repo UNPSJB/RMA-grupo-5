3d407caf45bee98731bd9d832a2cd7730bd1c211
Relokko <151683203+Relokko@users.noreply.github.com>
2024-11-07 00:02:26 -0300

    Update index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -8,15 +8,16 @@ import "assets/scss/argon-dashboard-react.scss";
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
+import Register from "views/examples/Register";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
     <Routes>
+      <Route path="/*" element={<Navigate to="/auth/register" replace/>} />
       <Route path="/admin/*" element={<AdminLayout />} />
       <Route path="/auth/*" element={<AuthLayout />} />
-      <Route path="*" element={<Navigate to="/admin/index" replace />} />
     </Routes>
   </BrowserRouter>
 );
